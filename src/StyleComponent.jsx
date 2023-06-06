b15e4b4f2c73fe914207d732afdaa32be5595448
BurakKurt <krtburak@outlook.com>
2023-06-06 17:25:05 +0300

    status modified styled-componentd

--- src/StyleComponent.jsx
+++ src/StyleComponent.jsx
@@ -8,7 +8,6 @@ export const AnimalCard = styled.div`
   margin: 2rem auto;
   padding: 1rem 0;
   background-color: #C4DFDF;
-  s
   border-radius: 15px;
   font-family: 'Roboto', sans-serif;
   letter-spacing: 1px;
@@ -27,7 +26,7 @@ export const AnimalImageFrame = styled.div`
   width: 300px;
   height: 300px;
   border-radius: 15px;
-  box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.5);
+  box-shadow: 0 0 12px rgba(0, 0, 0, 0.5);
 `;
 
 export const AnimalImage = styled.img`
@@ -41,4 +40,15 @@ export const AnimalImage = styled.img`
 export const AnimalContent = styled.p`
   color: #526D82;
   max-width: 70%;
+`;
+
+export const StatusFrame = styled.div`
+  position: fixed;
+  width: 500px;
+  right: 1rem;
+  bottom: 1rem;
+  padding: 1rem;
+  border-radius: 15px;
+  background-color: rgb(39, 55, 77, 0.6);
+  color: #F9F5F6;
 `;
--- src/status.jsx
+++ src/status.jsx
@@ -1,10 +1,12 @@
-const Status = ({ status }) => {
+import { StatusFrame } from "./StyleComponent";
+
+const Status = () => {
 
   return (
-    <div className={"position-fixed end-0 start-0 bottom-0 m-2 bg-danger py-2 px-3 rounded text-white"}>Şu
-      anda <span className="fw-bold">{process.env.NODE_ENV}</span> modundasınız.
+    <StatusFrame>Şu
+      anda <span className="fw-bold text-info">{process.env.NODE_ENV}</span> modundasınız.
       <p className="m-0">{process.env.REACT_APP_CONTENT}</p>
-    </div>
+    </StatusFrame>
   );
 };
 

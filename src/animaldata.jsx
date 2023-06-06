bfe8d469a3ae1a75199d4c77b7862d51e414600b
BurakKurt <krtburak@outlook.com>
2023-06-06 17:03:09 +0300

    data

--- src/animaldata.jsx
+++ src/animaldata.jsx
@@ -4,11 +4,30 @@ export const AnimalData = [
     animal: "Kedi",
     content: "Kediler, insanlarla binlerce yıldır süren bir arkadaşlık ve evcil hayvan ilişkisi içerisindedir. Hem evde hem de sokaklarda yaşayan kediler, birçok insanın gönlünde taht kurmuş sevimli ve meraklı yaratıklardır.Kediler, genellikle bağımsızlıkları ve zarafetleriyle tanınırlar. Uzun, kısa, tüylü veya tüysüz çeşitli türleri vardır ve her biri kendi özelliklerine sahiptir. Kedilerin çeşitli renklerde ve desenlerde tüyleri bulunur, bu da onları daha da çekici kılar.",
     imageSrc: "/assets/media/cat.jpg"
-  },
-  {
+  }, {
     id: 1,
     animal: "Köpek",
-    content: "Kediler, insanlarla binlerce yıldır süren bir arkadaşlık ve evcil hayvan ilişkisi içerisindedir. Hem evde hem de sokaklarda yaşayan kediler, birçok insanın gönlünde taht kurmuş sevimli ve meraklı yaratıklardır.Kediler, genellikle bağımsızlıkları ve zarafetleriyle tanınırlar. Uzun, kısa, tüylü veya tüysüz çeşitli türleri vardır ve her biri kendi özelliklerine sahiptir. Kedilerin çeşitli renklerde ve desenlerde tüyleri bulunur, bu da onları daha da çekici kılar.",
+    content: "Köpekler, insanlar tarafından evcil hayvan olarak en yaygın olarak beslenen memelilerden biridir. Binlerce yıldır insanlarla birlikte yaşamaktadırlar ve insanlarla çok yakın bir ilişki kurmuşlardır. Köpekler, farklı ırklara ayrılan geniş bir tür çeşitliliğine sahiptir. Her bir ırk, farklı fiziksel özelliklere, davranışlara ve yeteneklere sahip olabilir. Örneğin, bazı köpek ırkları küçük boyutlarda ve oyuncuken, diğerleri büyük ve koruyucu olabilir.",
     imageSrc: "/assets/media/dog.jpg"
+  }, {
+    id: 2,
+    animal: "Sincap",
+    content: "Sincaplar, kemirgenler ailesine ait olan küçük memelilerdir. Dünya genelinde yaygın olarak bulunurlar ve çeşitli türleri vardır. Sincaplar, genellikle ağaçlarda yaşarlar ve ağaç dallarında hızlı ve çevik hareket edebilen yetenekli tırmanıcılar olarak bilinirler. Sincapların fiziksel özellikleri arasında büyük ve dikkat çekici gözlere, tüylü kuyruklara ve genellikle tüylü bir posta sahip olmaları yer alır. Kuyruklarını denge sağlamak, iletişim kurmak ve sıcaklık düzenlemek için kullanabilirler.",
+    imageSrc: "/assets/media/squirrel.jpg"
+  }, {
+    id: 3,
+    animal: "Timsah",
+    content: "Timsahlar, krokodil takımına ait büyük, sürüngen memelilerdir. Genellikle tropikal ve subtropikal bölgelerde, tatlı su kaynaklarında yaşarlar. Dünyada iki farklı timsah ailesi bulunur: gerçek timsahlar (Alligatoridae) ve timsahlar (Crocodylidae). Timsahların fiziksel özellikleri, güçlü çeneleri, sivri dişleri ve pullarla kaplı derileridir. Boyutları türlerine bağlı olarak değişir. Bazı timsah türleri oldukça büyük olabilir ve 6 metreye kadar uzunluğa ulaşabilir. Timsahların vücutları sucul ortamlara uyum sağlamıştır. Yüzgeç benzeri ayakları ve uzun kuyrukları, su içinde hızlı ve kontrol edilebilir hareket etmelerini sağlar.",
+    imageSrc: "/assets/media/crocodile.jpg"
+  }, {
+    id: 4,
+    animal: "Kokarca",
+    content: "Kokarcalar, keseliler (Marsupialia) takımında yer alan memeli hayvanlardır. Dünyada farklı türleri bulunur ve çoğunlukla Amerika kıtasında, özellikle Kuzey ve Güney Amerika'da yaşarlar.  Kokarcalar, karakteristik bir savunma mekanizması olan kötü kokulu bir sıvı püskürtme yetenekleriyle bilinirler. Tehlike hissettiklerinde, anüslerinden özel bir sıvı püskürtürler ve bu sıvının kokusu ve tadı oldukça kötüdür. Bu sıvı, potansiyel avcıları uzaklaştırmak için etkilidir.",
+    imageSrc: "/assets/media/skunk.jpg"
+  }, {
+    id: 5,
+    animal: "Gökdoğan",
+    content: "Gökdoğan kuşları, Avrupa, Asya ve Kuzey Amerika gibi bölgelerde bulunan orta boyutlu yırtıcı kuşlardır. Falconidae familyasına aittirler. Gökdoğanlar, güçlü kanatları ve keskin pençeleriyle tanınır ve hızlı ve çevik avcılardır. Gökdoğanların vücutları aerodinamik yapıdadır ve havada yüksek hızlarda süzülebilirler. Uzun kanatları ve kıçı çubuklu kuyrukları vardır, bu da onlara manevra kabiliyeti sağlar. Renkleri türlerine bağlı olarak değişebilir, ancak genellikle kahverengi veya gri tonlarda bulunurlar.",
+    imageSrc: "/assets/media/peregrine.jpg"
   }
 ];
